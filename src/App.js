89982741192c90482ff607db3acacb46446efb0a
Andrew Dzobko <mytyber@gmail.com>
2023-04-07 19:11:57 -0700

    Removed redundant Main component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import './style.css'
-import Main from './components/Main';
+import './style.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FloatingBullets from './components/FloatingBullets';
