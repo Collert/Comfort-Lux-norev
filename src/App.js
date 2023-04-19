7c2ae1c2fdbb7c462287ab184a62d3590d322d3f
Andrew Dzobko <mytyber@gmail.com>
2023-04-18 21:20:53 -0700

    FInished why us section

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import Hero from './components/Hero';
 import FloatingBullets from './components/FloatingBullets';
 import Gallery from './components/Gallery';
 import AboutUs from './components/AboutUs';
+import WhyUs from './components/WhyUs';
 
 function App() {
   return (
     <>
       <Header/>
-      <span className='nav-anchor' id='hero-nav'/>
       <Hero/>
-      <span className='nav-anchor' id='about-us-nav'/>
       <AboutUs/>
-      <span className='nav-anchor' id='five-bullet-points-nav'/>
       <FloatingBullets/>
-      <span className='nav-anchor' id='gallery-nav'/>
       <Gallery/>
+      <WhyUs/>
     </>
   )
 }
