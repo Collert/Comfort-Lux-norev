a60df7129e148d2113eaa5c4bbf8636dd858ab87
Andrew Dzobko <mytyber@gmail.com>
2023-04-19 13:05:01 -0700

    Restructured layout and changed text

--- src/components/FloatingBullets.jsx
+++ src/components/FloatingBullets.jsx
@@ -11,26 +11,25 @@ export default function FloatingBullets () {
                         <div>
                             <ul>
                                 <li>Zabezpieczenie</li>
-                                <li>Demontaż Całej Łazienki</li>
-                                <li>Skucie Starej Posadzki</li>
-                                <li>Wyburzenie Ścian Działowych</li>
-                                <li>Demontaż Mebli</li>
-                                <li>Demontaż Mebli Bez Uszkodzenia</li>
-                                <li>Demontaż Glazury - Terakoty</li>
-                                <li>Zerwanie Starej Farby</li>
-                                <li>Zerwanie Tynków</li>
-                                <li>Poszerzenie Otworów W Ścianach</li>
-                                <li>Demontaż Deski Podłogowej</li>
-                                <li>Demontaż Paneli</li>
-                                <li>Demontaż Sufitu Podwieszanego</li>
-                                <li>Demontaż Drzwi</li>
-                                <li>Demontaż Futryn</li>
-                                <li>Demontaż Płyt GK Ze Ścian</li>
-                                <li>Demontaż Karniszy</li>
-                                <li>Demontaż Lamp</li>
-                                <li>Zerwanie Tapet</li>
-                                <li>Wynos Gruzu</li>
-                                <li>Kontener Na Gruz</li>
+                                <li>Demontaż całej łazienki</li>
+                                <li>Skucie starej posadzki</li>
+                                <li>Wyburzenie ścian działowych</li>
+                                <li>Demontaż mebli</li>
+                                <li>Demontaż glazury - terakoty</li>
+                                <li>Zerwanie starej farby</li>
+                                <li>Zerwanie tynków</li>
+                                <li>Poszerzenie otworów w ścianach</li>
+                                <li>Demontaż deski podłogowej</li>
+                                <li>Demontaż paneli</li>
+                                <li>Demontaż sufitu podwieszanego</li>
+                                <li>Demontaż drzwi</li>
+                                <li>Demontaż futryn</li>
+                                <li>Demontaż okien</li>
+                                <li>Demontaż płyt GK ze ścian</li>
+                                <li>Demontaż karniszy</li>
+                                <li>Demontaż lamp</li>
+                                <li>Zerwanie tapet</li>
+                                <li>Wynos gruzu</li>
                             </ul>
                         </div>
                     </div>
@@ -41,107 +40,109 @@ export default function FloatingBullets () {
                         <div><h3>Prace Malarskie</h3></div>
                         <div>
                             <ul>
-                                <li>Szlifowanieścian Przed Rozpoczęciem Prac Malarskich</li>
-                                <li>Naprawa Pęknieć</li>
-                                <li>Wzmocnienie Ścian Śiatką Z Tynkiem</li>
-                                <li>Naprawa Bruzd Po Elektryce - Hydraulice</li>
+                                <li>Szlifowanie ścian przed rozpoczęciem prac malarskich</li>
+                                <li>Naprawa pęknięć</li>
+                                <li>Wzmocnienie ścian siatką z tynkiem</li>
+                                <li>Naprawa bruzd po elektryce/hydraulice</li>
                                 <li>Gruntowanie</li>
-                                <li>Wyrównanie Ścian-Sufitu Tynkie (Punktowo)</li>
-                                <li>Tynkowanie Ścian Po Listwach Prowadzączych</li>
+                                <li>Wyrównanie ścian-sufitu tynkiem</li>
+                                <li>Tynkowanie ścian po listwach prowadzących</li>
                                 <li>Gładzie</li>
-                                <li>Gładz Dekoracyjna</li>
-                                <li>Malowanie (Na Biało)</li>
-                                <li>Malowanie (Kolorem)</li>
+                                <li>Gładź dekoracyjna</li>
                                 <li>Akryl</li>
-                                <li>Malowanie Farbą Olejową</li>
-                                <li>Poprawki Malarskie</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="bullet-column">
-                <img id="brush-illustration" alt="golden paintbrush" src="/ai_illustrations/brush.png"/>
                 <div className="floating-bullet">
                     <div className="wrapper">
-                        <div><span className="material-symbols-outlined">bathtub</span></div>
-                        <div><h3>Łazienka, kuchnia</h3></div>
+                        <div><span className="material-symbols-outlined">construction</span></div>
+                        <div><h3>Budowa / Przebudowa - Praca W Pomieszczeniach</h3></div>
                         <div>
                             <ul>
-                                <li>Wylewka Betonowa</li>
-                                <li>Wylewka Samopozomująca</li>
-                                <li>Podłoga Z Keramzytu I Płyty Farmacel</li>
-                                <li>Montaż + Podłączenie Ogrzewania Podłogowego (Rodzaj: Elektrycze)</li>
-                                <li>Hydroizolacja</li>
-                                <li>Układanie Płytek O Standarowych Wymiarach</li>
-                                <li>Układanie Płytek Wieloformatowych</li>
-                                <li>Układanie Płytek O Małych Formatach</li>
-                                <li>Zrobienie Półek Z Płytek</li>
-                                <li>Cięcie Płytek Pod Kątem 45 (Płytka Wieloformatowa)</li>
-                                <li>Cięcie Płytek Pod Kątem 45 (Płytka O Standardowych Wymiarach Oraz Maloformatowa Płytka)</li>
-                                <li>Wywiercanie Otworów W Płytce</li>
-                                <li>Fugowanie Jako Odzielny Element</li>
-                                <li>Zabudowa Stelażu WD / BD</li>
-                                <li>Zabudowa Rur (Pionowych - Poziomych)</li>
-                                <li>Zabudowa Rur Z Rewizą Pod Liczniki Wody</li>
-                                <li>Zabudowa Wanny</li>
-                                <li>Zabudowa Rur Od Wentylacji</li>
-                                <li>Zabudowa Pod Półkie</li>
-                                <li>Zabudowa Wanny Z Wnęką Na Stopy</li>
-                                <li>Montaż Sufitu Podwieszanego Na Powierzchi O Małej Ilości M2 (Pojedycze Płytowanie , Bez Wypełnienia)</li>
-                                <li>Montaż Sufitu Podwieszanego Na Powierzchi O Małej Ilości M2 (Podwójne Płytowanie Z Wypełnieniem)</li>
-                                <li>Montaz Sufitu Podwieszanego Typu "ARMSTRONG"</li>
-                                <li>Montaż Sufitu Podwieszanego (Konstrukcja Artestyczna)</li>
-                                <li>Wygłuszenie Ścian</li>
-                                <li>Montaż Zlewu</li>
-                                <li>Montaż Umywalki</li>
-                                <li>Montaż + Podłączenie Kranu</li>
-                                <li>Montaż Umywalki Z Szafką</li>
-                                <li>Montaż WC</li>
-                                <li>Montaż BD</li>
-                                <li>Montaż Grzejnika</li>
-                                <li>Montaż Zestawu Natynkowego</li>
-                                <li>Montaż Pojedyczych Elementów Natynkowych</li>
-                                <li>Montaż Lustra</li>
-                                <li>Montaż Kabiny Prysznicowej</li>
-                                <li>Montaż Wanny</li>
-                                <li>Montaż Oświetlenia (O Standardowych Wymiarach)</li>
-                                <li>Montaż Gniazdek / Włączników</li>
-                                <li>montaż szafek</li>
-                                <li>Składanie Szafek</li>
-                                <li>Montaż + Docięcie Blatu</li>
-                                <li>Podłączenie Piekarnika</li>
-                                <li>Podłączenie Płyty Indukcyjnej</li>
-                                <li>Podłączenie Zmywarki</li>
-                                <li>Podłączenie Pralki</li>
-                                <li>Montaż + Podłączenie Okapu</li>
-                                <li>Silikony</li>
-                                <li>Akryl</li>
-                                <li>Gładzie (Na Powierzchni O Małej Ilości M2)</li>
-                                <li>Malowanie (Na Powierzchni O Małej Ilości M2)</li>
+                                <li>Murowanie ścian</li>
+                                <li>Przedścianki z profili i płyt GK (pojedyncze lub podwójne płytowanie)</li>
+                                <li>Sufit podwieszany z profili i płyt GK (pojedyncze lub podwójne płytowanie)</li>
+                                <li>Montaż płyt GK na klej</li>
+                                <li>Sufit powieszany (styl artystyczny)</li>
+                                <li>Układanie paneli</li>
+                                <li>Wzmocnienie ścian</li>
+                                <li>Montaż cokołów</li>
+                                <li>Wzmocnienie nadproża</li>
+                                <li>Montaż listew maskujących</li>
+                                <li>Montaż listew przysufitowych</li>
+                                <li>Montaż drzwi + ościeżnicy</li>
+                                <li>Montaż karniszy</li>
+                                <li>Montaż oświetlenia o różnych wymiarach</li>
+                                <li>Montaż parapetów</li>
+                                <li>Montaż mocowania dla telewizora</li>
+                                <li>Montaż telewizora</li>
+                                <li>Montaż elementów do zajęć sportowych</li>
+                                <li>Wbudowanie peszla pod telewizor w ścianę</li>
+                                <li>Przyklejenie tapety</li>
                             </ul>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="bullet-column">
+                <img id="brush-illustration" alt="golden paintbrush" src="/ai_illustrations/brush.png"/>
                 <div className="floating-bullet">
                     <div className="wrapper">
                         <div><span className="material-symbols-outlined">bolt</span></div>
                         <div><h3>Elektryka</h3></div>
                         <div>
                             <ul>
-                                <li>Nowe Pónkty Elektryki</li>
-                                <li>Doprowadzenie Kabli</li>
-                                <li>Tworzenie Puszek Rozdzielczych</li>
-                                <li>Podłączenie Skrzyńki Elektrycznej</li>
-                                <li>Podłączenie + Montaż Oświtlenia Led</li>
-                                <li>Doprowadzenie Kabli Rtv</li>
-                                <li>Podłązenie Domofonu</li>
-                                <li>Podączenie Tv</li>
-                                <li>Podłączenie Klimatyzacji</li>
-                                <li>Podłączenie Sprzetu Agd</li>
-                                <li>Podłączenie Żaluzi Elektryczych</li>
+                                <li>Nowe punkty elektryczne</li>
+                                <li>Doprowadzenie kabli</li>
+                                <li>Tworzenie puszek rozdzielczych</li>
+                                <li>Podłączenie skrzynki elektrycznej</li>
+                                <li>Podłączenie + montaż oświetlenia LED</li>
+                                <li>Doprowadzenie kabli RTV</li>
+                                <li>Podłączenie domofonu</li>
+                                <li>Podłączenie TV</li>
+                                <li>Podłączenie klimatyzacji</li>
+                                <li>Podłączenie sprzętu AGD</li>
+                                <li>Podłączenie żaluzji elektrycznych</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bullet-column">
+                <div className="floating-bullet">
+                    <div className="wrapper">
+                        <div><span className="material-symbols-outlined">bathtub</span></div>
+                        <div><h3>Łazienka, kuchnia</h3></div>
+                        <div>
+                            <ul>
+                                <li>Wylewka betonowa</li>
+                                <li>Wylewka samopoziomująca</li>
+                                <li>Podłoga z keramzytu i płyty farmacell</li>
+                                <li>Montaż + podłączenie ogrzewania podłogowego (rodzaj: elektryczne)</li>
+                                <li>Hydroizolacja</li>
+                                <li>Układanie płytek o standardowych wymiarach, wieloformatowych</li>
+                                <li>Zrobienie półek z płytek</li>
+                                <li>Cięcie płytek pod kątem 45</li>
+                                <li>Wywiercanie otworów w płytce</li>
+                                <li>Montaż/zabudowa stelażu WC / BD</li>
+                                <li>Zabudowa rur (pionowych - poziomych)</li>
+                                <li>Zabudowa rur z rewizją pod liczniki wody</li>
+                                <li>Zabudowa wanny</li>
+                                <li>Zabudowa rur od wentylacji</li>
+                                <li>Zabudowa pod półki</li>
+                                <li>Zabudowa wanny z wnęką na stopy</li>
+                                <li>Montaż sufitu podwieszanego</li>
+                                <li>Wygłuszenie ścian .</li>
+                                <li>Montaż zlewu, umywalki, kranu, grzejnika</li>
+                                <li>Montaż zestawu natynkowego</li>
+                                <li>Montaż kabiny prysznicowej, wanny</li>
+                                <li>Montaż gniazdek, włączników, oświetlenia</li>
+                                <li>Montaż/składanie szafek</li>
+                                <li>Montaż + docięcie blatu</li>
+                                <li>Podłączenie piekarnika, płyty indukcyjnej, okapu, zmywarki, pralki</li>
+                                <li>Gładzie</li>
+                                <li>Malowanie</li>
                             </ul>
                         </div>
                     </div>
@@ -152,13 +153,13 @@ export default function FloatingBullets () {
                         <div><h3>Hydraulika</h3></div>
                         <div>
                             <ul>
-                                <li>Zmiana Układu Hydraulicznego W Łazience</li>
-                                <li>Zmiana Układu Hydrauliczego W Kuchni</li>
-                                <li>Zmiana Podejścia Pod Grzejnik</li>
-                                <li>Nowe Pónkty Żimnej-Ciepłej Wody</li>
-                                <li>Montaż Stelażu Wc</li>
-                                <li>Montaż Stelażu Bd</li>
-                                <li>Praze Hydraulicze Po Zalaniu</li>
+                                <li>Zmiana układu hydraulicznego w łazience</li>
+                                <li>Zmiana układu hydraulicznego w kuchni</li>
+                                <li>Zmiana podejścia pod grzejnik</li>
+                                <li>Nowe punkty zimnej/ciepłej wody</li>
+                                <li>Montaż stelażu WC</li>
+                                <li>Montaż stelażu BD</li>
+                                <li>Prace hydrauliczne po zalaniu</li>
                             </ul>
                         </div>
                     </div>
