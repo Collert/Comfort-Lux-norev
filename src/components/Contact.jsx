760f028ba805728474bd0149146718341104d28e
Andrew Dzobko <mytyber@gmail.com>
2023-04-20 23:23:17 -0700

    Touched up the footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,10 +21,10 @@ export default function Contact () {
             <div className="info">
                 <img src="./branding/logo/original/logo-transparent.png" alt="logo"/>
                 <h3>Сomfort Lux Sp. Z o.o.</h3>
-                NIP 5252804077<br/>
-                Adres: ul. Marszałkowska 58/15, 00-545 Warszawa<br/>
-                Email: <a href="mailto:karmarina046@gmail.com">karmarina046@gmail.com</a><br/>
-                Nt.tel: <a href="tel:+787836406">787836406</a>
+                {/* NIP 5252804077<br/> */}
+                <div className="contact-info-piece">ul. Marszałkowska 58/15, 00-545 Warszawa  <span class="material-symbols-outlined footer">place</span></div>
+                <div className="contact-info-piece"><a href="mailto:karmarina046@gmail.com">karmarina046@gmail.com</a> <span class="material-symbols-outlined footer">email</span></div>
+                <div className="contact-info-piece"><a href="tel:+787836406">787836406</a> <span class="material-symbols-outlined footer">call</span></div>
             </div>
         </section>
     )
