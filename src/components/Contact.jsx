e622c38cea8442e73bb305c68a451d4b7559351a
Andrew Dzobko <mytyber@gmail.com>
2023-06-08 22:35:24 -0700

    Implemented a proper form submission gateway

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,14 @@ import { useInView } from "react-intersection-observer";
 export default function Contact (props) {
 
     const { ref, inView } = useInView();
+    const [formData, setFormData]  =React.useState({
+        name:"",
+        phone:"",
+        email:"",
+        district:"",
+        comment:""
+    })
+    const [formFilled, setFormFilled] = React.useState(false)
 
     React.useEffect(()=>{
         props.setVisibleSections(prev=>({
@@ -16,18 +24,47 @@ export default function Contact (props) {
 
     function onSubmit(e) {
         e.preventDefault()
+        const url = "https://script.google.com/macros/s/AKfycbyNs3FR3WWNFK_GeR_oVcLfu7xP8IUuzsYIItnx8IMo7CV_LuuWaGONFvCjl7tehvxC/exec"
+        setFormFilled(true)
+        fetch(url, {
+            method:"POST",
+            body: new FormData(document.querySelector("#form"))
+        })
+        setTimeout(() => {
+            setFormData({
+                name:"",
+                phone:"",
+                email:"",
+                district:"",
+                comment:""
+            })
+        }, 1000);
+        setTimeout(() => {
+            setFormFilled(false)
+        }, 5000);
+    }
+
+    const thankYouBanner = (
+        <div id="thank-you-banner">
+            <span>Dziękuję!<br/> Skontaktujemy się z wami wkrótce.</span>
+        </div>
+    )
+
+    function handleChange(e) {
+        setFormData((prev) => ({...prev, [e.target.name]:e.target.value}))
     }
 
     return (
         <section ref={ref} id="contact">
-            <form onSubmit={onSubmit}>
+            <form id="form" onSubmit={onSubmit}>
                 <h2>Umów się na<br/> bezpłatną wycenę!</h2>
-                <input type="text" placeholder="Imię"/>
-                <input type="tel" placeholder="Numer telefonu"/>
-                <input type="email" placeholder="E-mail"/>
-                <input type="text" placeholder="Okolica"/>
-                <textarea placeholder="Komentarz"></textarea>
+                <input onChange={handleChange} value={formData.name} name="name" type="text" placeholder="Imię"/>
+                <input onChange={handleChange} value={formData.phone} name="phone" type="tel" placeholder="Numer telefonu"/>
+                <input onChange={handleChange} value={formData.email} name="email" type="email" placeholder="E-mail"/>
+                <input onChange={handleChange} value={formData.district} name="district" type="text" placeholder="Okolica"/>
+                <textarea onChange={handleChange} value={formData.comment} name="comment" placeholder="Komentarz"></textarea>
                 <button className="accent">WYŚLIJ</button>
+                {formFilled && thankYouBanner}
             </form>
             <div className="info">
                 <img src="./branding/logo/original/logo-transparent.png" alt="logo"/>
